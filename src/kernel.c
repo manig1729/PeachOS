7ca40bedba8888e356f6d5bf28a3db825907c1da
manig1729 <manikandang1729@gmail.com>
2021-08-30 22:35:31 +0530

    Change structure of the page table and include paging demonstration

--- src/kernel.c
+++ src/kernel.c
@@ -77,8 +77,20 @@ void kernel_main() {
     // Switch to kernel paging chunk
     paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
 
+    char* ptr = kzalloc(4096);
+    paging_set(paging_4gb_chunk_get_directory(kernel_chunk), (void*)0x1000, (uint32_t)ptr | PAGING_ACCESS_FROM_ALL | PAGING_IS_PRESENT | PAGING_IS_WRITABLE);
+
     // Enable paging
     enable_paging();
+
+    char* ptr2 = (char*) 0x1000;
+    ptr2[0] = 'A';
+    ptr2[1] = 'B';
+
+    print ("\n");
+    print(ptr2);    // sees virtual 0x1000 resolves it to the ptr address
+    print ("\n");
+    print(ptr);
 
     // Enables system interrupts
     enable_interrupts();
