4bb9dfcf8ae58a4fbd075bd4f10fa527848a53c4
manig1729 <manikandang1729@gmail.com>
2021-09-12 12:20:10 +0530

    Create a disk stream

--- src/kernel.c
+++ src/kernel.c
@@ -8,6 +8,7 @@
 #include "disk/disk.h"
 #include "fs/pparser.h"
 #include "string/string.h"
+#include "disk/streamer.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_row = 0;
@@ -81,8 +82,10 @@ void kernel_main() {
     // Enables system interrupts
     enable_interrupts();
 
-    struct path_root* root_path = pathparser_parse("0:/bin/mani/shell.exe", NULL);
-    if (root_path) {
+    struct disk_stream* stream = diskstreamer_new(0);
+    diskstreamer_seek(stream, 0x201);
+    unsigned char c = 0;
+    diskstreamer_read(stream, &c, 1);
 
-    }
+    while (1) {}
 }
