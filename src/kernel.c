f5e95632c7f5af9d4af92bbf37e1c99a15a272a0
manig1729 <manikandang1729@gmail.com>
2021-10-07 23:39:16 +0530

    Writing gdt using C instead of in boot.asm

--- src/kernel.c
+++ src/kernel.c
@@ -5,11 +5,14 @@
 #include "io/io.h"
 #include "memory/heap/kheap.h"
 #include "memory/paging/paging.h"
+#include "memory/memory.h"
 #include "disk/disk.h"
 #include "fs/pparser.h"
 #include "string/string.h"
 #include "disk/streamer.h"
 #include "fs/file.h"
+#include "gdt/gdt.h"
+#include "config.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_row = 0;
@@ -63,9 +66,22 @@ void panic (const char* msg) {
     while(1) {}
 }
 
+struct gdt gdt_real[PEACHOS_TOTAL_GDT_SEGMENTS];
+struct gdt_structured gdt_structured[PEACHOS_TOTAL_GDT_SEGMENTS] = {
+    {.base = 0x00, .limit = 0x00, .type = 0x00},         // NULL segment
+    {.base = 0x00, .limit = 0xFFFFFFFF, .type = 0x9A},   // Kernel code segment
+    {.base = 0x00, .limit = 0xFFFFFFFF, .type = 0x92}    // Kernel data segment
+};
+
 void kernel_main() {
     terminal_initialize();
     print("Hello World!\nI am Manikandan\n");
+
+    memset(gdt_real, 0x00, sizeof(gdt_real));
+    gdt_structured_to_gdt(gdt_real, gdt_structured, PEACHOS_TOTAL_GDT_SEGMENTS);
+
+    // Load the gdt
+    gdt_load(gdt_real, sizeof(gdt_real));
 
     // Initialise the heap
     kheap_init();
